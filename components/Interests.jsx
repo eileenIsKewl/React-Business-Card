c7a33a2b9549b3ce5a52508f3789da3fb1de6a0c
eileenIsKewl <eileenbusiness123@gmail.com>
2026-09-21 19:50:46 -0400

    Adjustments

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <>
             <h2>Interets</h2>
-            <p class="description">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="description" id="interests-desc">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </>
     )
 }
